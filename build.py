d6bc189109d8238558f7b404bdacf2551c4321e9
= <ethanmacdonald04@gmail.com>
2017-04-26 15:10:48 -0300

    initial

---
+++ build.py
@@ -0,0 +1,30 @@
+import os
+
+compiler = ""
+compiler = "g++"
+
+options = "-g"
+
+src_dir = "./src"
+bin_dir = "./bin"
+
+if not os.path.exists(bin_dir):
+    os.makedirs(bin_dir)
+
+command = "{} {} -o {}/compiled.out -std=c++14 -Iinclude/".format(compiler, options, bin_dir)
+
+for dirpath, dirnames, filenames in os.walk(src_dir):
+    for file in [f for f in filenames]:
+        if file.endswith(".cpp"):
+            print("{}/{}...".format(dirpath, file))
+            command = "{} {}/{} ".format(command, dirpath, file)
+
+if os.system("{}".format(command)):
+    #failed build
+    print("BUILD: Cannot Run - Build errors")
+else:
+    #success!
+    print("BUILD: Running...")
+    os.system("./bin/compiled.out")
+
+print("BUILD: Build complete")
